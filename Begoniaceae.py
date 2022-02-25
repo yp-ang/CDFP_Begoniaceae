e4bbe574043ef8bf33500cdba4a5be663d727264
yp-ang <angyupin159753@gmail.com>
2022-02-25 17:30:47 +0800

    able to filter out species list

--- Begoniaceae.py
+++ Begoniaceae.py
@@ -1,6 +1,7 @@
 from urllib.request import Request, urlopen
 from io import StringIO
 from html.parser import HTMLParser
+import xlsxwriter
 
 class MLStripper(HTMLParser):
     def __init__(self):
@@ -29,13 +30,16 @@ def write_line(filepath, content):
     with open(filepath, 'a', encoding="utf-8") as f:
         f.write(content)
 
+#def function(filepath, line):
+
+
 
 if __name__ == '__main__':
 
     url = "https://www.philippineplants.org/Families/Begoniaceae.html"
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     Genus = "Begonia"
-    filepath = r"C:\Users\angyu\Desktop\Begaoniaceae.txt"
+    filepath = r"C:\Users\angyu\Desktop\Begoniaceae.txt"
 
     web_byte = urlopen(req).read()
     webpage = strip_tags(web_byte.decode('latin-1'))
@@ -46,6 +50,9 @@ if __name__ == '__main__':
         f.write(" ")
         f.close()
 
+    excel = xlsxwriter.Workbook(r"C:\Users\angyu\Desktop\Begoniaceae.xlsx")
+    excelsheet = excel.add_worksheet("Begonias")
+    row = 0
 
     start_index = find_start(lines, Genus+" ")
     for i in range(start_index, fileLength):
@@ -53,11 +60,20 @@ if __name__ == '__main__':
         if (line.strip() != ""):
             if Genus + " Resource" in line:
                 break
-            elif Genus +" " in line:
-                write_line(filepath, "\n"+ line.lstrip() + "\n")
-                print("\n"+ line + "\n")
+            elif line.split()[0] == "Begonia":
+                edited_line = "\n" + line.lstrip() + " "
+                write_line(filepath, edited_line)
+                excelsheet.write(row, 0, line.lstrip())
+                row += 1
             else:
-                write_line(filepath, line.lstrip().rstrip() )
+                edited_line = line.lstrip().rstrip()
+                write_line(filepath, edited_line )
+
+
+
+    excel.close()
+
+
 
 
 
